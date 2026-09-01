4780dd03afbdb92e3656907c4746c9a1a9960eda
Bryan Steve Arellano Ruiz <bsr.arellano@yavirac.edu.ec>
2026-08-31 20:41:45 -0500

    feat: agregando el modulo de [Aministracón de Videos]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { VideosComponent } from './videos/videos'; // Asegúrate de que apunte al archivo correcto de videos
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'videos', component: VideosComponent },
+  { path: '', redirectTo: 'videos', pathMatch: 'full' },
+  { path: '**', redirectTo: 'videos' }
+];
